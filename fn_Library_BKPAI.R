91707819a55d57dd0db2f85e79dcfa2195d7adb7
snandi <nandi@stat.wisc.edu>
2014-12-29 17:50:25 -0600

    Added chi square tables for factors

--- fn_Library_BKPAI.R
+++ fn_Library_BKPAI.R
@@ -29,3 +29,22 @@ Mode <- function(x) {
   ux[which.max(tabulate(match(x, ux)))]
 }
 ########################################################################
+
+fn_printFactorTables <- function(Col, Data1){
+  Colname <- gsub(pattern = "_", replacement = " ", x = colnames(Data1)[Col])
+  Caption.1 <- paste('Frequency table between', Colname, 'and Living Arrangement')
+  Caption.2 <- paste('Test of independence between', Colname, 'and Living Arrangement')
+  
+  Table.pct <- prop.table(table(Data1[,Col],Data1$live_arrange_max),1)
+  print(xtable(Table.pct*100, caption=Caption.1), table.placement='H')
+  Table.pct.r <- melt(Table.pct, id=rownames(Table.pct))
+  #table(Data1$poor_health,Data1$live_arrange_max)
+  Test <- chisq.test(table(Data1[,Col],Data1$live_arrange_max))
+  print(xtable(as.data.frame(cbind(X_Sq=Test$statistic, 
+                                   DF=Test$parameter, 
+                                   pvalue=Test$p.value)),
+               caption=Caption.2,
+               digits=c(0, 2, 0, 4)), 
+        table.placement='H', include.rownames=FALSE)  
+}
+
